6e54470cd7368db76b2f5c10861ba5690fabe144
Calves <calves.dev@gmail.com>
2025-03-07 18:46:43 -0300

    Desafio mestre concluído

--- xadrez.c
+++ xadrez.c
@@ -1,46 +1,62 @@
 // Desafio Movimentação de Peças de Xadrez
 #include <stdio.h>
 
+void movimentaPeca(int casas, char *direcao)
+
+{
+    if (casas > 0)
+    {
+        printf("%s\n", direcao);
+        movimentaPeca(casas - 1, direcao);
+    }
+}
+
+void movimentaBispo(int casas, char *vertical, char *horizontal)
+{
+    int j;
+    for (int i = 0; i < casas; i++)
+    {
+        printf("%s\n", vertical);
+        for (j = 0; j < 1; j++)
+        {
+            printf("%s\n", horizontal);
+        }
+        j--;
+    }
+}
+
+void movimentaCavalo(char *direcao1, char *direcao2)
+{
+    int casas = 3;
+    for (int i = 0; i < casas; i++)
+    {
+        i < (casas - 1) ? printf("Cima\n") : printf("Direita\n");
+    }
+}
+
+// Utilize loops aninhados com múltiplas variáveis e/ou condições para
+// simular o movimento do Cavalo em "L" (duas casas para cima e uma para a direita).
+
 int main()
 {
     /*Código que simula movimentação de peças de xadrez usando
     laços de repetição usando um laço diferente para cada peça*/
 
-    int casasTorre = 0, casasBispo = 0, casasRainha = 8, casasCavalo = 1; // Variável de controle dos loops while, do-while e for
-
-    printf("Torre:\n");
     // Loop da Torre
-    while (casasTorre < 5)
-    {
-        printf("Direita\n");
-        casasTorre++;
-    }
+    printf("Torre:\n");
+    movimentaPeca(5, "Direita");
 
-    printf("\nBispo:\n");
-    // Loop do Bispo
-    do
-    {
-        printf("Cima, Direita\n");
-        casasBispo++;
-    } while (casasBispo < 5);
-
-    printf("\nRainha:\n");
     // Loop da Rainha
-    for (int i; i < casasRainha; i++)
-    {
-        printf("Esquerda\n");
-    }
+    printf("\nRainha:\n");
+    movimentaPeca(8, "Esquerda");
 
-    printf("\nCavalo:\n");
+    // Loop do Bispo 5
+    printf("\nBispo:\n");
+    movimentaBispo(5, "Cima", "Direita");
+
     // Loop do Cavalo
-    for (int i = 0; i < casasCavalo; i++)
-    {
-        printf("Cima, ");
-        for (int j = 0; j < casasCavalo; j++)
-        {
-            printf("Esquerda, Esquerda\n");
-        }
-    }
+    printf("\nCavalo:\n");
+    movimentaCavalo("Cima", "Direita");
 
     return 0;
 }

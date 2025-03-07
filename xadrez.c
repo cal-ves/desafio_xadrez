0a453fb59e0b9735c526d2faa415c3f1628dd62c
Calves <calves.dev@gmail.com>
2025-03-07 16:46:07 -0300

    Desafio aventureiro concluído

--- xadrez.c
+++ xadrez.c
@@ -6,9 +6,9 @@ int main()
     /*Código que simula movimentação de peças de xadrez usando
     laços de repetição usando um laço diferente para cada peça*/
 
-    int casasTorre = 0, casasBispo = 0, casasRainha = 8; // Variável de controle dos loops while, do-while e for
+    int casasTorre = 0, casasBispo = 0, casasRainha = 8, casasCavalo = 1; // Variável de controle dos loops while, do-while e for
 
-    printf("\nTorre:\n");
+    printf("Torre:\n");
     // Loop da Torre
     while (casasTorre < 5)
     {
@@ -16,7 +16,7 @@ int main()
         casasTorre++;
     }
 
-    printf("Bispo:\n");
+    printf("\nBispo:\n");
     // Loop do Bispo
     do
     {
@@ -24,11 +24,22 @@ int main()
         casasBispo++;
     } while (casasBispo < 5);
 
-    printf("Rainha:\n");
+    printf("\nRainha:\n");
     // Loop da Rainha
     for (int i; i < casasRainha; i++)
     {
         printf("Esquerda\n");
+    }
+
+    printf("\nCavalo:\n");
+    // Loop do Cavalo
+    for (int i = 0; i < casasCavalo; i++)
+    {
+        printf("Cima, ");
+        for (int j = 0; j < casasCavalo; j++)
+        {
+            printf("Esquerda, Esquerda\n");
+        }
     }
 
     return 0;
